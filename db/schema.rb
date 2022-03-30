02a2b2bb44405a94c478c04d877da29618f25d89
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-30 19:34:20 +0200

    feat(model) : ajout du model Order

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_30_171701) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_30_173344) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
     t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.date "date"
+    t.integer "order_id"
+    t.integer "customer_id"
+    t.string "country"
+    t.string "product_code"
+    t.string "product_description"
+    t.integer "quantity"
+    t.float "unit_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

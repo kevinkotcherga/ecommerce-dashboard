a18bda9ccb6c2937490d3ee8e2d40ebc281aed11
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-30 19:38:05 +0200

    feat(controller) : ajout du la méthode download_csv dans orders_controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,10 @@
 
 # app/controllers/order_controller.rb
 class OrdersController < ApplicationController
+  def download_csv
+    csv_file = File.join Rails.root, 'db', 'memory-tech-challenge-data.csv'
+    # AddOrderWorker est appellé depuis workers/add_order_worker
+    # perfom_async permet de transmettre les données à Redis
+    AddOrderWorker.perform_async(csv_file)
+  end
 end

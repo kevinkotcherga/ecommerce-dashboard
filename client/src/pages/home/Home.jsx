60350a393e973648f458f4811b9683cf84d9abcf
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 11:14:12 +0200

    feat(pages) : ajout de style.scss au selecteur country

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../../components/header/Header'
 import styled from "styled-components";
+import './home.scss'
 
 const Select = styled.select`
   padding: 10px;

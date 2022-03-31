c3e76ced141f80c9f0424c59eee7a3b457f2c4d8
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 10:44:02 +0200

    refactor(react) : suppresion des fichiers non utilisés pour le projet

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,5 @@
-import './App.css';
-import React, { useEffect } from 'react';
 
 function App() {
-
-  useEffect(() => {
-    fetch('/notes')
-      .then(res => res.json())
-      .then(data => console.log(data));
-  }, []);
 
   return (
     <div className="App">

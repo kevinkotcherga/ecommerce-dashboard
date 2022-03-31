e8e7a2687c1fdbef07f0d133680cc7702bd7bd12
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 09:58:21 +0200

    feat(views) : ajout du dosier search et de la view search.json

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 # app/controllers/search_controller.rb
-class MainController < ApplicationController
+class SearchController < ApplicationController
   # force_json force la requête pour être au format json
   before_action :force_json, only: :search
   def index; end
 
+  # ransack recherche la valeur country avec la key :q dans Order
+  # @orders est envoyé dans views/search/search.json pour être lu
   def search
     @orders = Order.ransack(country_cont: params[:q])
                       .result(distinct: true)
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :notes, only: [:index]
   get '/orders/download_csv'
   get '/orders/index'
+  get :search, controller: :search
+
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

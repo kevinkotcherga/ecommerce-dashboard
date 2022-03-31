0601a7b7141aa23f30a1b1253611fd6557b5965c
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 11:12:23 +0200

    feat(pages) : ajout d'un selecteur de pays

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import Header from '../../components/header/Header'
+import styled from "styled-components";
+
+const Select = styled.select`
+  padding: 10px;
+  font-size: 1rem;
+  font-weight: 500;
+`
+
+const Option = styled.option`
+
+`
 
 const Home = () => {
   return (
     <div className='home'>
       <Header />
+      <div className="home__country">
+        <span className='home__label'>Country</span>
+        <Select name="country">
+          <Option defaultValue >
+            All
+          </Option>
+          <Option>France</Option>
+          <Option>Norway</Option>
+          <Option>Germany</Option>
+          <Option>Netherlands</Option>
+          <Option>Australia</Option>
+          <Option>EIRE</Option>
+          <Option>United</Option>
+        </Select>
+      </div>
     </div>
   )
 }

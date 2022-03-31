efe4e3e3627f8fd61322c13a10a7f7666c8a8431
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 09:44:26 +0200

    feat(controller) : ajout de la méthode privée force_json à search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,18 @@
 
 # app/controllers/search_controller.rb
 class MainController < ApplicationController
+  # force_json force la requête pour être au format json
+  before_action :force_json, only: :search
   def index; end
 
   def search
     @orders = Order.ransack(country_cont: params[:q])
                       .result(distinct: true)
+  end
+
+  private
+
+  def force_json
+    request.format = :json
   end
 end

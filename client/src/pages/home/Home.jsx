6e99f3985558bca22533ac1ff9c0c0c113030627
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 11:05:52 +0200

    feat(components) : ajout du component Header

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../../components/header/Header'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className='home'>
+      <Header />
     </div>
   )
 }

e2cc46cfe1e2490cb534025d2587b5bf18090e77
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-03-31 09:40:18 +0200

    feat(controller) : ajout du controller search

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :notes, only: [:index]
   get '/orders/download_csv'
+  get '/orders/index'
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
